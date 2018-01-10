03d5de7474cd3f665a800a08bfb6106595bf0b0a
Manickavasagam6066 <35221332+Manickavasagam6066@users.noreply.github.com>
2018-01-10 16:10:21 +0530

    Create n.c

---
+++ n.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a>0)
+	printf("number is postive");
+	if(a<0)
+	printf("number is negative");
+	if(a==0)
+	printf("number is zero");
+}
