aa6b96a4a7a40b08c0e79554778de38c2b11bf0d
Manickavasagam6066 <35221332+Manickavasagam6066@users.noreply.github.com>
2018-01-21 06:15:37 +0000

    Create 3.c

---
+++ set3/3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int a,b[a],i,min;
+scanf("%d",&a);
+for(i=0;i<a;i++)
+{
+scanf("%d",&b[i]);
+if(i==0)
+min=b[i];
+if(min>b[i])
+min=b[i];
+}
+printf("%d",min);
+return 0;
+}
