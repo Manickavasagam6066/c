394b175261f937d9521d4da8bba0fe59b4f8894a
Manickavasagam6066 <35221332+Manickavasagam6066@users.noreply.github.com>
2018-01-24 16:52:56 +0530

    Create 1.c

---
+++ set4/1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	char as[1000];
+	int b=0,c=0,i;
+	gets(a);
+	for(i=0;as[i]!=NULL;i++)
+	{
+		if((as[i]>='a'&&a[i]<='z')||(as[i]>='A'&&as[i]<='Z'))
+		b=++c;
+	}
+	printf("%d",b);
+	return 0;
+}
