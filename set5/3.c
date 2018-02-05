19ac7ef69f9611d54fc3aaf05c98e2fa496b57de
Manickavasagam6066 <35221332+Manickavasagam6066@users.noreply.github.com>
2018-02-05 16:47:21 +0530

    Create 3.c

---
+++ set5/3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+int main() 
+{
+	char x[20],y[20];
+	printf("\n e the 2 strings");
+	scanf("%s %s",&x,&y);
+	printf("\n concadenated string is %s%s",x,y);
+	return 0;
+}
