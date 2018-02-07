070c67f9b64f834a027c51676fbab47d547484e9
Manickavasagam6066 <35221332+Manickavasagam6066@users.noreply.github.com>
2018-02-07 16:53:44 +0530

    Create 9.c

---
+++ set5/9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main () 
+{
+int n1, n2,sum;
+printf ("\nenter 1st numbers\t");
+scanf ("%d", &n1);
+printf ("\nenter 2st numbers\t");
+scanf ("%d", &n2);
+sum=n1+n2;
+printf ("\nthe sum value is\t%d",sum);
+}
