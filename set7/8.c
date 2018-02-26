31b090f259d747ba2f65c5054773346d246b7702
Manickavasagam6066 <35221332+Manickavasagam6066@users.noreply.github.com>
2018-02-26 16:41:34 +0530

    Create 8.c

---
+++ set7/8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+	int i=1,n;
+	printf("enter the number\n");
+	scanf("%d",&n);
+	if((n%7)==0)
+	    {
+	      i=2;
+	   }
+	if(i==2)
+	printf("yes");
+	else
+	printf("no");
+}
